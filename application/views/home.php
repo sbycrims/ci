f29b46c25f71f529f3b4fabb58ca981246eccd97
Ali Elsayed <unique.business.sw@gmail.com>
2017-06-15 11:38:03 +0200

    Init Repo
    Codeigniter 3.14
    public folder
    bootstrap
    css
    js
    fontawsome
    
    create a basic home controller

---
+++ application/views/home.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('no access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?= base_url('public/bootstrap/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('public/font-awesome/css/font-awesome-animation.min.css') ?>">
+    <!-- jQuery -->
+    <script src="<?= base_url('public/js/jquery.min.js')?>"></script>
+    <script src="<?= base_url('public/bootstrap/js/bootstrap.min.js')?>"></script>
+    
+    <!-- style sheets -->
+    <link rel="stylesheet" href="<?= base_url('public/css/form-elements.css')?>"/>
+    <link rel="stylesheet" href="<?= base_url('public/css/style.css')?>"/>
+</head>
+<body>
+    
+</body>
+</html>
